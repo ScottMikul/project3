f0e07aa5f4dcc49ddb76c066311cbf46090d15d1
ScottMikul <Scott.p.mikul@gmail.com>
2017-03-25 00:11:00 -0400

    Whoops. The tests got a bit repetitive.

--- src/test/java/com/teamtreehouse/techdegree/overboard/test/UserTest.java
+++ src/test/java/com/teamtreehouse/techdegree/overboard/test/UserTest.java
@@ -17,6 +17,8 @@ import static org.junit.Assert.*;
 public class UserTest {
     private Board board;
     private User Scotty, Craig, comedian, PeanutGallery, SillySally;
+    private Question joke;
+    private Answer punchline;
     @Before
     public void setUp(){
          board = new Board("BadJoke");
@@ -25,27 +27,24 @@ public class UserTest {
          PeanutGallery = board.createUser("Peanut gallery");
          comedian = board.createUser("comedian");
          SillySally = board.createUser("SillySally");
+         joke = Scotty.askQuestion("How many South Americans does it take to change a lightbulb?");
+         punchline = comedian.answerQuestion(joke,"A Brazilian");
     }
 
     @Test
     public void UserReputationIncrementsByFiveOnQuestionUpvote(){
-        Question joke = Scotty.askQuestion("How many South Americans does it take to change a lightbulb?");
         Craig.upVote(joke);
         assertEquals(Scotty.getReputation(),5);
     }
 
     @Test
     public void UserReputationIncrementsByTenOnAnswerUpvote(){
-        Question joke = Scotty.askQuestion("How many South Americans does it take to change a lightbulb?");
-        Answer punchline = comedian.answerQuestion(joke,"A Brazilian");
         SillySally.upVote(punchline);
         assertEquals(comedian.getReputation(),10);
     }
 
     @Test
     public void UserReputationIncrementsByFifteenOnAcceptedAnswer(){
-        Question joke = Scotty.askQuestion("How many South Americans does it take to change a lightbulb?");
-        Answer punchline = comedian.answerQuestion(joke,"A Brazilian");
         Scotty.acceptAnswer(punchline);
         assertEquals(comedian.getReputation(),15);
 
@@ -53,42 +52,32 @@ public class UserTest {
 
     @Test(expected = VotingException.class)
     public void UserCanNotUpvoteOwnAnswer(){
-        Question joke = Scotty.askQuestion("How many South Americans does it take to change a lightbulb?");
-        Answer punchline = comedian.answerQuestion(joke,"A Brazilian");
         comedian.upVote(punchline);
     }
 
     @Test(expected = VotingException.class)
     public void UserCanNotUpvoteOwnQuestion(){
-        Question joke = Scotty.askQuestion("How many South Americans does it take to change a lightbulb?");
         Scotty.upVote(joke);
     }
 
     @Test(expected = VotingException.class)
     public void UserCanNotDownvoteOwnAnswer(){
-        Question joke = Scotty.askQuestion("How many South Americans does it take to change a lightbulb?");
-        Answer punchline = comedian.answerQuestion(joke,"A Brazilian");
         comedian.downVote(punchline);
     }
 
     @Test(expected = VotingException.class)
     public void UserCanNotDownvoteOwnQuestion(){
-        Question joke = Scotty.askQuestion("How many South Americans does it take to change a lightbulb?");
         Scotty.downVote(joke);
     }
 
     @Test(expected = AnswerAcceptanceException.class)
     public void UserCanNotAcceptAnswerForAnotherUserQuestion() throws Exception {
-        Question joke = Scotty.askQuestion("How many South Americans does it take to change a lightbulb?");
-        Answer punchline = comedian.answerQuestion(joke,"A Brazilian");
         comedian.acceptAnswer(punchline);
 
     }
 
     @Test
     public void DownVotingAnAnswerRemovesOneReputation() throws Exception {
-        Question joke = Scotty.askQuestion("How many South Americans does it take to change a lightbulb?");
-        Answer punchline = comedian.answerQuestion(joke,"A Brazilian");
         Scotty.acceptAnswer(punchline);
         assertEquals(comedian.getReputation(),15);
         PeanutGallery.downVote(punchline);
